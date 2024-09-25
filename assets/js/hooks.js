781952dd64ab9648fbba148a399e5d9d145d1b8f
florinhoinarescu <florin_hoinarescu@yahoo.com>
2024-09-25 16:13:03 +0300

    update

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -3,23 +3,8 @@ hooks = {
 
     targetFunctions: [
         { func:"addNote", cb: [] },
-        {
-            func:"addTag",
-            cb: [
-                (functionName, result, args) => {
-                console.log(functionName)
-                },
-            ]
-            
-        },
-        {
-            func:"addCat",
-            cb: [
-                (functionName, result, args) => {
-                console.log(functionName)
-                }
-            ]
-        }
+        { func:"addTag", cb: [] },
+        { func:"addCat", cb: [] }
     ],
 
     addAction: (whereToAdd, action ) => {
@@ -34,7 +19,26 @@ hooks = {
             event: 'Add_Custom_Note' 
         };
         fireGTMTag(gtmObject);
+    },
+
+    addTagAction: (functionName, result, args, ...extraArgs) => {
+        const userToken = Cookies.get(settings.user.userTokenCookie);
+        gtmObject = {
+            userToken: userToken,
+            event: 'Add_Custom_Tag' 
+        };
+        fireGTMTag(gtmObject);
+    },
+    addCatAction: (functionName, result, args, ...extraArgs) => {
+        const userToken = Cookies.get(settings.user.userTokenCookie);
+        gtmObject = {
+            userToken: userToken,
+            event: 'Add_Custom_Cat' 
+        };
+        fireGTMTag(gtmObject);
     }
 }
 
 hooks.addAction('addNote', bindArgsAtEnd(hooks.addNoteAction, []));
+hooks.addAction('addTag', bindArgsAtEnd(hooks.addTagAction, []));
+hooks.addAction('addTag', bindArgsAtEnd(hooks.addCatAction, []));
