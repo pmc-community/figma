6a079e74563c934122ecce3c13908146b6416683
florinhoinarescu <florin_hoinarescu@yahoo.com>
2025-04-05 15:07:02 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,7 +62,7 @@ window.customiseTheme = (pageObj = null) => {
         setAnonymousUserToken();
         if (gData.gtm.enabled) pushInfoToGTM(pageInfo);
 
-        // do some corrections of JTD search, scrolling is not working well
+        // do some corrections of JTD search, because scrolling is not working well
         // do some translation of dynamic search elements
         // do some styling for better view (mostly on mobile)
         correctJTDSearch();
