966147d722a1ca1304e12d9c4a2e7078cfd30406
florinhoinarescu <florin_hoinarescu@yahoo.com>
2025-04-07 19:43:56 +0300

    update

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1448,8 +1448,13 @@ const applyColorSchemaCorrections = (theme=null) => {
     // apply color corrections on mobile
     if (preFlight.envInfo.device.deviceType === 'mobile') {
         textColor = $(settings.layouts.leftSideBar.siteTitle).css('color');
-        $(settings.layouts.leftSideBar.sideNav).css('background', $(settings.layouts.leftSideBar.header).css('background'));
-        $(settings.layouts.leftSideBar.link).css('cssText', `color: ${textColor} !important;`);
+        setTimeout(() => {
+            $(settings.layouts.leftSideBar.sideNav)
+                .css('background', '')
+                .css('cssText', `background: ${$(settings.layouts.leftSideBar.header).css('background')} !important`);
+            $(settings.layouts.leftSideBar.link).css('cssText', `color: ${textColor} !important;`);
+        }, 100); // small delay is needed here otherwise the backgtound is not set correctly (mostly on firefox mobile)
+        
         $(settings.layouts.leftSideBar.mobileMenuBtnIcon).css('cssText', `color: ${textColor} !important;`);
         $(settings.layouts.leftSideBar.mobileNavLinkExpander).css('cssText', `color: ${textColor} !important;`);
     }
